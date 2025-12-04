fbc8ded8ff58bd1251895d5976be95e59cef1859
HailuBen <99948206+HailuBen@users.noreply.github.com>
2025-12-04 17:43:44 -0500

    Implement session management and user context
    Added session management and user context for views.

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const session = require('express-session');
 const mongoose = require('mongoose');
 const movieRoutes = require('./routes/movies');
+const authRoutes = require('./routes/auth');
 const methodOverride = require('method-override');
 const app = express();
 
@@ -14,12 +16,27 @@ app.use(express.json());
 app.use(express.static('public'));
 app.use(methodOverride('_method'));
 
+// session setup
+app.use(session({
+    secret: 'mySecretKey',
+    resave: false,
+    saveUninitialized: false
+}));
+
+// make user available in all views
+app.use((req, res, next) => {
+    res.locals.currentUser = req.session.user;
+    next();
+});
+
 // have the routes below with one another
 app.use('/movies', movieRoutes);
+app.use('/', authRoutes);
 
 // heres the home route
+// home route
 app.get('/', (req, res) => {
-  res.redirect('/movies');
+  res.render('home', { title: 'Welcome' });
 });
 
 // added my own mongodb for the sake of simplicity; feel free to change it. user/pass are in the link
